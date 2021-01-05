e2af72e0344b14d8a6e2d170f46188b7704633ba
Tuan Lee <me@vlee.dev>
2021-01-05 13:53:10 +0700

    Add docker login

--- run.sh
+++ run.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
+# This shell script is used for github action
 workDir="$(pwd)"
 pullAndPush(){
   # alpine:3.12.3
   local imageAndTag=$1
   docker pull "${imageAndTag}"
 }
+# Docker login
+# new login with new container registry url and PAT
+echo "${{ secrets.CR_PAT }}" | docker login ghcr.io -u "${GITHUB_ACTOR}" --password-stdin
 # Generate all cache files
 find "${workDir}/cache" -type f -name "*.txt" | while IFS= read -r cacheFile
 do
