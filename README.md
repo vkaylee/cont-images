449af067aea6fe4b8f6414162253eb1fc5d1de1c
Tuan Lee <me@vlee.dev>
2021-01-05 15:08:46 +0700

    Improve

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # cont-images
+## List Mariadb
+    - asd
+    - asd
+    - asd
+## List Mariadb
+    - asd
+    - asd
+    - asd
--- run.sh
+++ run.sh
@@ -1,23 +1,55 @@
 #!/usr/bin/env bash
 # This shell script is used for github action
 workDir="$(pwd)"
-pullAndPush(){
+
+pullAndPush() {
+  local dockerImageAndTag=$1
+  local githubImageAndTag=$2
+
+  docker pull "${dockerImageAndTag}"
+  docker tag "${dockerImageAndTag}" "${githubImageAndTag}"
+  docker push "${githubImageAndTag}"
+}
+run() {
   # alpine:3.12.3
-  local imageAndTag=$1
-  githubImageAndTag="ghcr.io/${GITHUB_REPOSITORY}:$(echo "${imageAndTag}" | tr ":" "-")"
-  # If it does not exist in github registry
-  if ! docker pull "${githubImageAndTag}"; then
-    # If it exists in docker hub
-    if docker pull "${imageAndTag}"; then
-      docker tag "${imageAndTag}" "${githubImageAndTag}"
-      docker push "${githubImageAndTag}"
-    fi
+  local dockerImageAndTag
+  dockerImageAndTag=$1
+  local githubImageAndTag
+  githubImageAndTag="ghcr.io/${GITHUB_REPOSITORY}:$(tr ":" "-" "${dockerImageAndTag}")"
+
+  local isDockerImageExisted=false
+  local isGithubImageExisted=false
+  # Check existed
+  if docker pull "${githubImageAndTag}"; then
+    isGithubImageExisted=true
   fi
+  if docker pull "${dockerImageAndTag}"; then
+    isDockerImageExisted=true
+  fi
+  # Pull and push
+  if [ "${isGithubImageExisted}" = false ] && [ "${isDockerImageExisted}" = true ]; then
+    pullAndPush "${dockerImageAndTag}" "${githubImageAndTag}"
+  fi
+  # generate readme
+  if [ "${isGithubImageExisted}" = true ]; then
+    generateReadme "${dockerImageAndTag}" "${githubImageAndTag}"
+  fi
+
+}
+generateReadme() {
+  local dockerImageAndTag=$1
+  local githubImageAndTag=$2
+  tee README.md << END
+## List Mariadb
+  - asd
+  - asd
+  - asd
+END
+  cat README.md
 }
 # Generate all cache files
-find "${workDir}/cache" -type f -name "*.txt" | while IFS= read -r cacheFile
-do
-  for i in $(<"${cacheFile}");do
-    pullAndPush "$i"
+find "${workDir}/cache" -type f -name "*.txt" | while IFS= read -r cacheFile; do
+  for i in $(<"${cacheFile}"); do
+    run "$i"
   done
 done
