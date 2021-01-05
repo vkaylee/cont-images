beefb51e234c7f0c9a6f4b93e3b64d1a64708b30
Tuan Lee <me@vlee.dev>
2021-01-05 14:18:04 +0700

    Add pull and push

--- run.sh
+++ run.sh
@@ -4,7 +4,15 @@ workDir="$(pwd)"
 pullAndPush(){
   # alpine:3.12.3
   local imageAndTag=$1
-  docker pull "${imageAndTag}"
+  githubImageAndTag="ghcr.io/${GITHUB_REPOSITORY}:$(echo "${imageAndTag}" | tr ":" "-")"
+  # If it does not exist in github registry
+  if ! docker pull "${githubImageAndTag}"; then
+    # If it exists in docker hub
+    if docker pull "${imageAndTag}"; then
+      docker tag "${imageAndTag}" "${githubImageAndTag}"
+      docker push "${githubImageAndTag}"
+    fi
+  fi
 }
 # Generate all cache files
 find "${workDir}/cache" -type f -name "*.txt" | while IFS= read -r cacheFile
